86550846ef4122ae499284461fe5908cadd2b1e6
Kyla Joiner <kylajoiner@yahoo.com>
2022-08-19 15:28:02 -0700

    edit pet and delete pet completed

--- src/controllers/petController.ts
+++ src/controllers/petController.ts
@@ -31,3 +31,48 @@ export const addPet: RequestHandler = async (req, res, next) => {
     await Pet.create(newPet);
     res.redirect('/pets');
 }
+
+export const editPetPage: RequestHandler = async (req, res, next) => {
+    let petId = req.params.petId;
+    let selectedPet: Pet | null = await Pet.findOne({
+        where: { petId }
+    })
+
+    if (selectedPet) {
+        res.render('editPet', { foundPet: selectedPet });
+    }
+    else {
+        res.status(404).render('error', { message: 'Uh-Oh! Pet was not found!' });
+    }
+}
+
+export const editPet: RequestHandler = async (req, res, next) => {
+    let petId = req.params.petId;
+    let updatedPet: Pet = req.body;
+
+    let [updated] = await Pet.update(updatedPet, {
+        where: { petId }
+    });
+
+    if (updated === 1) {
+        res.redirect('/pets');
+    }
+    else {
+        res.render('error', { message: 'Pet could not be updated' });
+    }
+}
+
+export const deletePet: RequestHandler = async (req, res, next) => {
+    let petId = req.params.petId;
+
+    let deleted = await Pet.destroy({
+        where: { petId }
+    });
+
+    if (deleted) {
+        res.redirect('/pets');
+    }
+    else {
+        res.status(404).render('error', { message: 'Cannot find pet' });
+    }
+}
--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addPet, addPetPage, allPets, onePet } from "../controllers/petController";
+import { addPet, addPetPage, allPets, deletePet, editPet, editPetPage, onePet } from "../controllers/petController";
 
 const router = Router();
 
@@ -9,7 +9,11 @@ router.get('/add', addPetPage);
 
 router.post('/add', addPet);
 
+router.get('/edit/:petId', editPetPage);
 
+router.post('/edit/:petId', editPet);
+
+router.post('/delete/:coffeeId', deletePet);
 
 router.get('/:petId', onePet);
 
